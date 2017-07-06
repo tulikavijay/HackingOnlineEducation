68829f193c917d554c5acb7ec3cd8d59ccac7557
tux <tulikavijay54@gmail.com>
2017-07-06 14:05:43 +0530

    Changes to urls.py ; for debugging optionswe can see static files

--- Books/urls.py
+++ Books/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+     urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
