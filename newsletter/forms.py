7f6efbb631cc7de791f25d36261283e15937444d
tux <tulikavijay54@gmail.com>
2017-07-06 14:44:03 +0530

    Added form for SignUp

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,1 +1,15 @@
+from django import forms
 
+class SignUpForm(forms.ModelForm):
+    class Meta():
+        model=SignUp
+        fields=['full_name','email']
+
+    def clean_email(self):
+        email=self.cleaned_data.get('email')
+        #
+        return email
+    def clean_fullname(self):
+        full_name=self.cleaned_data.get('full_name')
+        #
+        return full_name
