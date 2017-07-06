27e53f012129c4b2a4b6a13fff9b42e540a0ec81
tux <tulikavijay54@gmail.com>
2017-07-06 14:21:48 +0530

    Added SignUp form for newsletter and made a blank forms.py

--- newsletter/models.py
+++ newsletter/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+#sign up form for newsletter not login or contact
+class SignUp(models.Model):
+     full_name=models.CharField(max_length=60,blank=True)
+     email=models.EmailField()
