590b0a174c5975ac8d4f1b880edf219dc4230af2
tux <tulikavijay54@gmail.com>
2017-07-06 15:27:46 +0530

    Made a view for signup request

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .forms import SignUpForm
 
 # Create your views here.
+def signup(request):
+    form=SignUpForm
+    instance=form.save(commit=False)
+    email=form.clean_email()
+    name=form.clean_fullname()
+    if form is_valid :
+        instance.save()
+        context{
+        'title':'Sign up for our newsletter'
+        'form' : form
+        }
+    return render(request,'signup.html',context)
