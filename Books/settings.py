181e7bca7cbb8bc20fa647504fc5fee6d2e069cf
tux <tulikavijay54@gmail.com>
2017-07-06 14:01:12 +0530

    Added Static and media folder and their locations

--- Books/settings.py
+++ Books/settings.py
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_PATH=os.path.join(BASE_DIR,'static')
+
+MEDIA_URL='/media/'
+MEDIA_PATH=os.path.join(BASE_DIR,'media')
